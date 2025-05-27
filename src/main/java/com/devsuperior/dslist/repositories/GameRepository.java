d093674b8bacc04f81ebbdaab5229bcea4f0aaaf
victor <victorcsales@gmail.com>
2025-05-27 12:25:54 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
 
